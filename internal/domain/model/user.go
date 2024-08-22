8b7b4e17d89dd9bd189c3a58f5ac688b9abbe52a
Angga Suwito <aqsuwito@pvg.co.id>
2024-08-22 20:56:09 +0700

    Finishing define size of varchar length database

--- internal/domain/model/user.go
+++ internal/domain/model/user.go
@@ -8,9 +8,7 @@ type User struct {
 	BaseModel
 	UserAccount []UserAccount `gorm:"foreignKey:UserID"`
 
-	Phone    string `gorm:"column:phone;size:100;"`
-	VANumber string `gorm:"column:va_number;size:100;"`
-	PIN      string `gorm:"column:pin;size:255;"`
-	Name     string `gorm:"column:name;size:255;"`
-	Email    string `gorm:"column:email;size:255;"`
+	Phone string `gorm:"column:phone;size:100;"`
+	Name  string `gorm:"column:name;size:255;"`
+	Email string `gorm:"column:email;size:255;"`
 }
--- internal/domain/model/user_account.go
+++ internal/domain/model/user_account.go
@@ -8,7 +8,9 @@ type UserAccount struct {
 	BaseModel
 	User User `gorm:"foreignKey:UserID"`
 
-	UserID  string `gorm:"column:user_id;size:36;"`
-	Balance int64  `gorm:"column:balance"`
-	Status  string `gorm:"column:status;size:100;"`
+	UserID   string `gorm:"column:user_id;size:36;"`
+	Balance  int64  `gorm:"column:balance"`
+	Status   string `gorm:"column:status;size:100;"`
+	VANumber string `gorm:"column:va_number;size:100;"`
+	PIN      string `gorm:"column:pin;size:255;"`
 }
